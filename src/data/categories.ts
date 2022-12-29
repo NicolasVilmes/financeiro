38c3c38dab933df041d9b74f970b266ee6c0ea37
Nicolas Vilmes <nicolasvilmes@outlook.com>
2022-12-29 00:26:08 -0300

    categories and items

---
+++ src/data/categories.ts
@@ -0,0 +1,8 @@
+import {Category} from "../types/category"
+
+export const categories: Category = {
+  food: { title: 'Alimentação', color: 'blue', expense: true },
+  rent: { title: 'Aluguel', color: 'Purple', expense: true },
+  salary: {
+    title: 'Salário', color: 'green', expense: true}
+}
---
+++ src/data/items.ts
@@ -0,0 +1,8 @@
+import { Item } from "../types/Item";
+
+export const items: Item[] = [
+  { date: new Date(2023, 0, 1), category: 'food', title: 'BK', value: 32.50 },
+  { date: new Date(2023, 0, 1), category: 'salary', title: 'Salario', value: 4500 },
+  { date: new Date(2023, 0, 1), category: 'rent', title: 'Aluguel', value: 2000 },
+  { date: new Date(2023, 0, 1), category: 'food', title: 'McDonauts', value: 32.50 },
+];
