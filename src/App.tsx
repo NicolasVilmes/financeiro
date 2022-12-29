2e799c97b03c474af46aeae68e25d2a4e3d62abc
Nicolas Vilmes <nicolasvilmes@outlook.com>
2022-12-29 00:04:13 -0300

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import * as C from "./app.styles";
+
+const App = () => {
+  return (
+    <C.Conteiner>
+      <C.Header>
+        <C.Headertext>Meu Balanço</C.Headertext>
+      </C.Header>
+      <C.body>
+        {}
+        {}
+        {}
+      </C.body>
+    </C.Conteiner>
+  );
+};
+
+export default App;
