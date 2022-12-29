1486c77f92eb4bf7f27d963edddb98d4406889c1
Nicolas Vilmes <nicolasvilmes@outlook.com>
2022-12-29 00:04:27 -0300

    First commit

---
+++ src/app.styles.ts
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+export const Conteiner = styled.div`
+`;
+
+export const Header = styled.div`
+    background-color: darkgreen;
+    height: 150px;
+    text-aling: center;
+`;
+
+export const Headertext = styled.h1`
+    margin: 0 0 0 40vw;
+    padding: 0;
+    color: #FFF;
+    padding-top: 30px
+`;
+
+export const body = styled.div`
+    margin: auto;
+    max-width: 980px
+    margin-bottom: 50px;
+`;
